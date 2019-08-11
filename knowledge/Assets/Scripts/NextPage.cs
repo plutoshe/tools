2a78dfa8bafa429d888a9492bbfd7ee55b458db3
plutoshe <plutoshe@gmail.com>
2019-08-11 12:40:12 +0800

    add turn next page initial effect

---
+++ knowledge/Assets/Scripts/NextPage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextPage : MonoBehaviour
+{
+    public GameObject m_GameobjectPageBack;
+    public int m_rotateAngle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, m_rotateAngle));
+        m_GameobjectPageBack.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+}
