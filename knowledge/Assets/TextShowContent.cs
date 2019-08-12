cc14404889b2e29ed02ab4d0b49c931db9b0d2fc
plutoshe <plutoshe@gmail.com>
2019-08-12 22:59:53 +0800

    add page move

--- knowledge/Assets/TextShowContent.cs
+++ knowledge/Assets/TextShowContent.cs
@@ -1,45 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class TextShowContent : MonoBehaviour
+public class TextShowContent : MonoBehaviour, IDragHandler, IBeginDragHandler
 {
-    private RectTransform m_content;
+    private RectTransform m_content, m_rect;
+    public float m_mouseSensitivity; 
+    private Vector3 m_moveOffset;
+
     // Start is called before the first frame update
     private void Awake()
     {
-
-
-        
-    }
-    private void Start()
-    {
+        m_rect = transform.GetComponent<RectTransform>();
         m_content = transform.Find("Content").GetComponent<RectTransform>();
-        //print(m_content.rect.yMax);
-        //print(m_content.rect.yMin);
-        //print(transform.GetComponent<RectTransform>().rect.height);
-        //print(transform.Find("Content").GetComponent<RectTransform>().rect.height);
-        //print(m_content.rect.height);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        m_moveOffset = Input.mousePosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        float maxOffsetY = m_content.rect.height - m_rect.rect.height - m_content.offsetMax.y;
+        float offsetY = Mathf.Min(maxOffsetY, (Input.mousePosition.y - m_moveOffset.y) * m_mouseSensitivity);
+        offsetY = Mathf.Max(-m_content.offsetMax.y, offsetY);
+        Vector2 newOffsetMin = m_content.offsetMin;
+        Vector2 newOffsetMax = m_content.offsetMax;
+        newOffsetMin.y += offsetY;
+        newOffsetMax.y += offsetY;
+        m_content.offsetMin = newOffsetMin;
+        m_content.offsetMax = newOffsetMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float offsetY = m_content.rect.height -
-            transform.GetComponent<RectTransform>().rect.height;
-
-        print(offsetY);
-        if (m_content.offsetMax.y == 0)
-        {
-            Vector2 newOffsetMin = m_content.offsetMin;
-            Vector2 newOffsetMax = m_content.offsetMax;
-            newOffsetMin.y += offsetY;
-            newOffsetMax.y += offsetY;
-            m_content.offsetMin = newOffsetMin;
-            m_content.offsetMax = newOffsetMax;
-        }
-        //m_content.localPosition = newPosition;
+        print(m_content.offsetMin);
+        print(m_content.offsetMax);
     }
 
 }
