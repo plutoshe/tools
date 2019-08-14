ac0f274d905bd6d16b9cccfd74aa6ade44a519fc
plutoshe <plutoshe@gmail.com>
2019-08-14 17:06:34 +0800

    udpate datetime setting and yaml read/write method

--- knowledge/Assets/Scripts/Class/Record.cs
+++ knowledge/Assets/Scripts/Class/Record.cs
@@ -1,15 +1,35 @@
 ﻿using System.Collections;
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class RecordInfo
 {
     public int ID { get; set; }
     public string Front { get; set; }
     public string Back { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime ReviewDate { get; set; }
+    public DateTime ReviewDegree { get; set; }
+    public List<string> Tags { get; set; }
 }
 
 public class RecordDataStorage
 {
     public List<RecordInfo> RecordList { get; set; }
+    public RecordDataStorage()
+    {
+        RecordList = new List<RecordInfo>();
+    }
+    public RecordDataStorage(List<RecordInfo> _recordList)
+    {
+        RecordList = _recordList;
+    }
+}
+
+public enum ReviewingStatus
+{
+    Remember = 0,
+    Forget,
+    Skip,
 }
--- knowledge/Assets/Scripts/DataManager.cs
+++ knowledge/Assets/Scripts/DataManager.cs
@@ -1,28 +1,100 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System;
 using UnityEngine;
 using YamlDotNet.Serialization;
 
 public class DataManager : MonoBehaviour
 {
-    public string m_RecordCurrentModificationFilePath = "RecordCurrentModification.yaml";
-    public string m_RecordDataStorageFilePath = "RecordDataStorage.yaml";
+    private string m_RecordCurrentModificationFilePath = "RecordCurrentModification.yaml";
+    private string m_RecordDataStorageFilePath = "RecordDataStorage.yaml";
+    private List<RecordInfo> m_reviewingRecords;
+    private int m_currentReviewRecordID;
+
+    private bool needReview(RecordInfo record)
+    {
+        return true;
+    }
+
+    private string GetDataPath(string filename)
+    {
+        return Path.Combine(Application.dataPath, Path.Combine("data", filename));
+    }
+
+    static public void Serializer<T>(string _filePath, T obj) 
+    {
+        StreamWriter yamlWriter = File.CreateText(_filePath);
+        Serializer yamlSerializer = new Serializer();
+        yamlSerializer.Serialize(yamlWriter, obj);
+        yamlWriter.Close();
+    }
+
+    static public T Deserializer<T>(string _filePath) 
+    {
+        if (!File.Exists(_filePath))
+        {
+            throw new FileNotFoundException();
+        }
+        StreamReader yamlReader = File.OpenText(_filePath);
+        Deserializer yamlDeserializer = new Deserializer();
+
+        T info = yamlDeserializer.Deserialize<T>(yamlReader);
+        yamlReader.Close();
+        return info;
+    }
+
+    private RecordDataStorage GetRecordDataStorage()
+    {
+        string dataPath = GetDataPath(m_RecordDataStorageFilePath);
+        return Deserializer<RecordDataStorage>(dataPath);
+    }
+
 
     private void Awake()
     {
-        string dataPath = Path.Combine(Application.dataPath, "data/" + m_RecordCurrentModificationFilePath);
-        print(Application.dataPath);
-        print(dataPath);
-        if (!File.Exists(dataPath))
-        {
-            return;
-        }
 
-        StreamReader yamlReader = File.OpenText(dataPath);
-        Deserializer yamlDeserializer = new Deserializer();
-        RecordDataStorage info = yamlDeserializer.Deserialize<RecordDataStorage>(yamlReader);
-        yamlReader.Close();
+        RecordDataStorage info = GetRecordDataStorage();
+
+        m_reviewingRecords = new List<RecordInfo>();
+        for (int i = 0; i < info.RecordList.Count; i++)
+        {
+            if (needReview(info.RecordList[i]))
+            {
+                m_reviewingRecords.Add(info.RecordList[i]);
+            }
+        }
+        m_currentReviewRecordID = 0;
+        m_reviewingRecords[0].CreateDate = DateTime.Now;
+        SaveReviewingRecords();
+    }
+
+    public void SaveReviewingRecords()
+    {
+        var reviewingPath = GetDataPath(m_RecordCurrentModificationFilePath);
+        Serializer(reviewingPath, new RecordDataStorage(m_reviewingRecords));
 
     }
+
+    public void ReviewNextRecord()
+    {
+        m_currentReviewRecordID++;
+    }
+
+    public void ModifyCurrentRecord(RecordInfo modifiedRecord)
+    {
+        m_reviewingRecords[m_currentReviewRecordID] = modifiedRecord;
+        SaveReviewingRecords();
+    }
+
+    public void UpdateCurrentReviewingRecord(ReviewingStatus status)
+    {
+        switch (status)
+        {
+            case ReviewingStatus.Forget:break;
+            case ReviewingStatus.Remember: break;
+        }
+        ReviewNextRecord();
+    }
+    
 }
